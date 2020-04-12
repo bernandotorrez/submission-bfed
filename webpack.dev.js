1eaf5a115d3dcaae94251913d02a3e6d77de97db
bernandotorrez <bernandotorrez4@gmail.com>
2020-04-12 23:19:21 +0700

    Update with Webpack

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const path = require('path');
+
+module.exports = merge(common, {
+    mode: "development",
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+        clientLogLevel: 'error'
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    }
+})
